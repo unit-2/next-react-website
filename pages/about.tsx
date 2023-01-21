b3b56156deb48f87e95de153bf08bb599d899f5a
unit-2 <unit2web@gmail.com>
2023-01-22 00:30:47 +0900

    Add pages

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import Hero from 'components/hero'
+
+export default function About() {
+	return (
+				<Hero />
+  )
+}
---
+++ pages/blog/index.tsx
@@ -0,0 +1,7 @@
+import Hero from 'components/hero'
+
+export default function Blog() {
+	return (
+				<Hero />
+  )
+}
