ace4d794f13976d6b1a93660d2032c036e98670e
unit-2 <unit2web@gmail.com>
2023-01-22 00:30:32 +0900

    Move Fragment and CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import Layout from 'components/layout'
 import type { AppProps } from 'next/app'
+import styles from 'styles/Home.module.css'
+import React from 'react'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import Hero from 'components/hero'
-import styles from 'styles/Home.module.css'
-import React from 'react'
 
 export default function Home() {
 	return (
